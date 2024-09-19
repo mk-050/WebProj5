6fe384dab0439e5ab3ad0efc87a2a549ff41ad7c
mk-050 <yoshikawa.0505@outlook.jp>
2024-09-19 21:28:33 +0900

    20ページまで

--- src/com/diworksdev/webproj5/action/HelloStrutsAction.java
+++ src/com/diworksdev/webproj5/action/HelloStrutsAction.java
@@ -11,16 +11,16 @@ import com.diworksdev.webproj5.dto.HelloStrutsDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class HelloStrutsAction extends ActionSupport implements SessionAware {
-	private List<HelloStrutsDTO> HelloStrutsDTOList = new ArrayList<HelloStrutsDTO>();
+	private List<HelloStrutsDTO> helloStrutsDTOList = new ArrayList<HelloStrutsDTO>();
 	private Map<String, Object> session;
 
 	public String execute() {
 		String ret = ERROR;
 		HelloStrutsDAO dao = new HelloStrutsDAO();
-		HelloStrutsDTOList = dao.select();
+		helloStrutsDTOList = dao.select();
 
-		if (HelloStrutsDTOList.size() > 0) {
-			session.put("helloStrutsDTOList", "helloStrutsDTOList");
+		if (helloStrutsDTOList.size() > 0) {
+			session.put("helloStrutsDTOList", helloStrutsDTOList);
 			ret = SUCCESS;
 		} else {
 			ret = ERROR;
